0fe74c413b7661cdbd51b94340ed58faaaddd69a
PleaBanshee <llewellynant@gmail.com>
2023-01-26 10:16:27 +0200

    Added Multiply and Division functions

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ const Subtract = (num1, num2) => {
 };
 
 Subtract(12, 12);
+
+const Multiply = (num1, num2) => {
+    console.log(`${num1} x ${num2} = ${num1 * num2}`);
+};
+
+Multiply(12, 12);
+
+const Divide = (num1, num2) => {
+    console.log(`${num1} / ${num2} = ${num1 / num2}`);
+};
+
+Divide(12, 12);
